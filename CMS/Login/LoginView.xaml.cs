4995661d34f9fc6844c8e9dff5e6270e282033df
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2018-11-10 13:20:30 +0200

    step 1

---
+++ CMS/Login/LoginView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CMS.Login
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show((DataContext as LoginViewModel)?.Email);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            (DataContext as LoginViewModel).Email += ".com";
+        }
+    }
+}
---
+++ CMS/Login/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace CMS.Login
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public LoginViewModel()
+        {
+            this.Email = "test@email.com";
+        }
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+    }
+
+}
