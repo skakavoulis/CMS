5b54dceef4e702b2af6f22e2f0c694b084928525
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2018-11-23 17:11:18 +0200

    ...

--- CMS.Host/Program.cs
+++ CMS.Host/Program.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using CMS.WCF.Services;
-using CMS.WCF.Services.Interfaces;
 
 namespace CMS.Host
 {
@@ -13,20 +11,8 @@ namespace CMS.Host
     {
         static void Main(string[] args)
         {
-            var host = new ServiceHost(typeof(ClientsService));
-            try
-            {
-                host.Open();
-                Console.WriteLine("Press Enter to exit");
-                Console.ReadLine();
-                host.Close();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-                host.Abort();
-                Console.ReadLine();
-            }
+            //var host = new ServiceHost...
+          
         }
     }
 }
