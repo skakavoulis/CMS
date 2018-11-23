89a3214e360156a89667e955b69f1262b0ffce14
Sotiris Kakavoulis <sotiriskakavoulis@gmail.com>
2018-11-23 19:50:25 +0200

    ...

--- CMS.Services/Clients/WCFClientService.cs
+++ CMS.Services/Clients/WCFClientService.cs
@@ -7,23 +7,21 @@ namespace CMS.Services.Clients
 {
     public class WCFClientService : IClientService
     {
-        private ClientsServiceClient _client;
         public string AuthToken { get; set; }
 
 
         public WCFClientService()
         {
-            _client = new ClientsServiceClient("WSHttpBinding_IClientsService");
         }
 
         public Task<Client[]> GetClientsForBranch(string branchId)
         {
-            return _client.GetClientsAsync(30);
+            return null;
         }
 
         public Task<Client> AddNewClient(Client newClient)
         {
-            return _client.AddClientAsync(newClient);
+            return null;
         }
     }
 }
